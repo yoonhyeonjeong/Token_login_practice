acda73fd26b32520649426adc261d9e7c2639d6f
chen4023 <ads4023@naver.com>
2024-08-27 13:33:56 +0900

    [Node.js] 4일차 과제 init

--- server.js
+++ server.js
@@ -41,15 +41,18 @@ app.post("/", (req, res) => {
     res.status(401).send("로그인 실패");
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
+    // 이곳에 코드를 작성하세요.
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
+    // 이곳에 코드를 작성하세요.
   }
 });
 
 // 클라이언트에서 get 요청을 받은 경우
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
-
+  // 이곳에 코드를 작성하세요.
   // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
+  // 이곳에 코드를 작성하세요.
   return;
 });
 
